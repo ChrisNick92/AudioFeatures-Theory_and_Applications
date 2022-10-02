fb0b977df8770b07e0449ae333ad495c5f966171
Christos Nikou <chrisnick92@gmail.com>
2022-10-02 14:16:47 +0300

    Description revised

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ import seaborn as sns
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import itertools
 import numpy as np
+import os
 
 def CustomCmap(low,top):
     r1,g1,b1 = low
@@ -111,4 +112,103 @@ def custom_split(features, labels, test_ratio = 0.15, print_progress = False,
             print(f"- Class {class_label}: Train|Test ---> {len(train_indices)}|{len(test_indices)}")
     return X_train, X_test, y_train, y_test
         
+        
+
+class conv_block(nn.Module):
+    def __init__(self, in_channels, out_channels,
+                 kernel_size = 5, stride = 1, padding = 0, bias = True):
+        super(conv_block,self).__init__()
+        
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels = in_channels, out_channels =  out_channels,
+                      kernel_size = kernel_size, stride = stride,
+                      padding = padding, bias = bias),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU()
+            )
+        
+    def forward(self,x): 
+        return self.conv(x)
+class CNN(nn.Module):
+    def __init__(self,in_channels = 3, out_channels= 10):
+        super(CNN,self).__init__()
+        self.backbone = nn.Sequential(
+            conv_block(in_channels=in_channels,
+                       out_channels=6, bias = False),
+            nn.AdaptiveMaxPool2d(output_size = 62),
+            conv_block(in_channels = 6, out_channels = 8,
+                       bias = False),
+            nn.AdaptiveMaxPool2d(output_size = 29),
+            conv_block(in_channels=8, out_channels=12,
+                       bias=False),
+            nn.AdaptiveAvgPool2d(output_size= 12),
+            conv_block(in_channels=12, out_channels=16,
+                       bias=False),
+            nn.AdaptiveAvgPool2d(output_size = 4),
+            nn.Flatten()
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(in_features=4*4*16, out_features=128, bias=False),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.Linear(in_features = 128, out_features = 64, bias = False),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.Linear(in_features = 64, out_features = 10),
+        )
+    
+    def forward(self,x):
+        x = self.backbone(x)
+        return self.classifier(x)
+
+import glob
+import cv2
+
+class cnn_dataset(Dataset):
+    def __init__(self, X, y):
+        self.X, self.y = X,y
+        self.features = []
+        for img_path in self.X:
+            img = cv2.imread(img_path)
+            img = cv2.resize(img, dsize = (128,128))
+            self.features.append(np.transpose(img, (2,0,1)))
+        
+    def __len__(self):
+        return len(self.y)
+    def __getitem__(self,idx):
+        img = self.features[idx] # Transpose from HxWxC --> CxHxW
+        return torch.tensor(img, dtype = torch.float32)/255, torch.tensor(self.y[idx], dtype = torch.int64)
+
+def split_images(imgs_path, ratios = [0.70, 0.15, 0.15]):
+    genres = list(os.walk(imgs_path))[0][1]
+    class_mapping = {"blues":0, "classical":1, "country":2, "disco":3,
+                     "hiphop": 4, "jazz": 5, "metal": 6, "pop": 7, "reggae": 8, "rock":9}
+    X_train, y_train, X_val, y_val, X_test, y_test = [], [], [], [], [], []
+    
+    for genre in genres:
+        imgs = np.array(list(glob.glob(os.path.join(imgs_path,
+                                           genre, "*.png"))))
+        idxs = np.arange(start =0, stop = len(imgs))
+        train_idxs = np.random.choice(a = idxs, replace = False,
+                                      size = int(ratios[0]*len(idxs)))
+        rest_idxs = np.setdiff1d(ar1 = idxs,
+                                 ar2 = train_idxs, assume_unique=True)
+        alpha = ratios[1]/ratios[2]
+        val_ratio = 1/(1+alpha)
+        val_idxs = np.random.choice(a = rest_idxs, replace =  False,
+                                    size = int(val_ratio*len(rest_idxs)))
+        test_idxs = np.setdiff1d(ar1 = rest_idxs, ar2 = val_idxs,
+                                 assume_unique=True)
+        y_train += [class_mapping[genre]]*len(train_idxs)
+        y_val += [class_mapping[genre]]*len(val_idxs)
+        y_test += [class_mapping[genre]]*len(test_idxs)
+        X_train += list(imgs[train_idxs])
+        X_val += list(imgs[val_idxs])
+        X_test += list(imgs[test_idxs])
+        
+    target_names = 10*[0]
+    for k,v in class_mapping.items():
+        target_names[v] = k
+    
+    return X_train, y_train, X_val, y_val, X_test, y_val, target_names
         
